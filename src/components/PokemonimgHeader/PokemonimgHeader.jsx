7bd2249027d33ba08dbf9791ff4f71a578b7ff35
Kamran <123491415+KAMRANANSARI01@users.noreply.github.com>
2023-08-17 16:07:12 +0530

    change logo

--- src/components/PokemonimgHeader/PokemonimgHeader.jsx
+++ src/components/PokemonimgHeader/PokemonimgHeader.jsx
@@ -3,7 +3,7 @@ function PokemonimgHeader (){
     return(
         <div className="mx-auto mt-[20px] flex justify-center items-center ">
         <Link to="/"> 
-        <img className=" h-[150px] w-[300px] lg:h-[150px] lg:w-[470px] rounded  " src="./src/img.png" alt="logo" />
+        <img className=" h-[150px] w-[300px] lg:h-[150px] lg:w-[470px] rounded  " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU8jvVy4kfa2ia_l4fYRcxhwlzyn5bweSTWhjUPiZY&s" alt="logo" />
         {/* <h1 className='text-5xl text-zinc-100'>Pokemon</h1> */}
         </Link>
        
