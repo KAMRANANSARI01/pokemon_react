d0b13dab485211a1d6820597b2b8882a54f7f93e
Kamran <123491415+KAMRANANSARI01@users.noreply.github.com>
2023-08-17 16:44:36 +0530

    final header img logo

--- src/components/PokemonimgHeader/PokemonimgHeader.jsx
+++ src/components/PokemonimgHeader/PokemonimgHeader.jsx
@@ -3,8 +3,8 @@ function PokemonimgHeader (){
     return(
         <div className="mx-auto mt-[20px] flex justify-center items-center ">
         <Link to="/"> 
-        <img className=" h-[150px] w-[300px] lg:h-[150px] lg:w-[470px] rounded  " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU8jvVy4kfa2ia_l4fYRcxhwlzyn5bweSTWhjUPiZY&s" alt="logo" />
-        {/* <h1 className='text-5xl text-zinc-100'>Pokemon</h1> */}
+        <img className=" h-[80px] w-[300px] lg:h-[80px] lg:w-[370px] rounded bg-transparent opacity-70 mb-8 " src="https://adanc.org/wp-content/uploads/2018/08/pokemon-go-1000x600.jpg.webp" alt="logo" />
+        {/* <img className=" h-[130px] w-[320px] lg:h-[130px] lg:w-[370px] rounded bg-transparent  mt-4 mb-8 " src="https://c4.wallpaperflare.com/wallpaper/827/674/341/pokemon-wallpaper-preview.jpg" alt="logo" /> */}
         </Link>
        
       </div>
