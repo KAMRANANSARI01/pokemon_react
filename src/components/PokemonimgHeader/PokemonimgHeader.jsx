b287e0980d6bba3791db4d21f64d70184a917eef
Kamran <123491415+KAMRANANSARI01@users.noreply.github.com>
2023-08-17 16:55:04 +0530

    final logo change

--- src/components/PokemonimgHeader/PokemonimgHeader.jsx
+++ src/components/PokemonimgHeader/PokemonimgHeader.jsx
@@ -3,7 +3,7 @@ function PokemonimgHeader (){
     return(
         <div className="mx-auto mt-[20px] flex justify-center items-center ">
         <Link to="/"> 
-        <img className=" h-[130px] w-[320px] lg:h-[130px] lg:w-[370px] rounded bg-transparent  mt-4 mb-8 " src="https://c4.wallpaperflare.com/wallpaper/827/674/341/pokemon-wallpaper-preview.jpg" alt="logo" />
+        <img className=" h-[80px] w-[300px] lg:h-[90px] lg:w-[340px] rounded bg-transparent  lg:mt-4 mb-8 " src="https://c4.wallpaperflare.com/wallpaper/827/674/341/pokemon-wallpaper-preview.jpg" alt="logo" />
         </Link>
        
       </div>
